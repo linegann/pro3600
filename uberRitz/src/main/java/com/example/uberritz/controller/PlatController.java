0d9e735aee3dd442254b2cda16f18407e0f32753
linegann <bilal.boumeslout@telecom-sudparis.eu>
2022-04-20 18:23:15 +0200

    a miracle has happened PlatController.java needed a @Controller instead of a @RestController

--- uberRitz/src/main/java/com/example/uberritz/controller/PlatController.java
+++ uberRitz/src/main/java/com/example/uberritz/controller/PlatController.java
@@ -3,6 +3,7 @@ package com.example.uberritz.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,13 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
 import com.example.uberritz.model.Plat;
 import com.example.uberritz.service.PlatService;
 
-@RestController
+@Controller
 public class PlatController {
 
     @Autowired
